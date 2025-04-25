a4341eac5cd99c48cb20ef6d222a692953696ff9
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-04-25 15:54:48 +0200

    libsystemd: Add missing stdint.h include to _sd-common.h with IWYU pragma
    IWYU can't detect the usage of INT64_MIN and INT64_MAX in the _SD_ENUM_FORCE_S64()
    macro so let's add an explicit pragma so that the include isn't removed.

--- src/systemd/_sd-common.h
+++ src/systemd/_sd-common.h
@@ -2,6 +2,8 @@
 #ifndef foosdcommonhfoo
 #define foosdcommonhfoo
 
+#include <stdint.h> // IWYU pragma: keep
+
 /***
   systemd is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published by
